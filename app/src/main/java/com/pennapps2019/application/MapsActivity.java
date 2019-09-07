2febd343348276fa40a818967e8fee37482e4086
Jeffrey Leung <jleung513@gmail.com>
2019-09-07 18:30:46 -0400

    Move camera location to Levine Hall

--- app/src/main/java/com/pennapps2019/application/MapsActivity.java
+++ app/src/main/java/com/pennapps2019/application/MapsActivity.java
@@ -40,8 +40,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng levineHall = new LatLng(39.952090, -75.190840);
+        mMap.addMarker(new MarkerOptions().position(levineHall).title("Marker at Levine Hall"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(levineHall));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(18));
     }
 }
