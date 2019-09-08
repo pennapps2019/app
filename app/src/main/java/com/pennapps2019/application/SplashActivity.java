72c4fcbb00172aa8b04b15658bd518d35ae3f95d
Jeffrey Leung <jleung513@gmail.com>
2019-09-08 02:59:10 -0400

    Remove navbar from splash screen
    Cleeeeeeeeeeeeeeeeean.

--- app/src/main/java/com/pennapps2019/application/SplashActivity.java
+++ app/src/main/java/com/pennapps2019/application/SplashActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
@@ -18,6 +19,12 @@ public class SplashActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Hide both the navigation bar and the status bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
