20593090ff7f7b35cfbc39a0b110695381c580a5
Jeffrey Leung <jleung513@gmail.com>
2019-09-08 02:51:29 -0400

    Add splash screen

---
+++ app/src/main/java/com/pennapps2019/application/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.pennapps2019.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int DELAY = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DELAY);
+    }
+}
