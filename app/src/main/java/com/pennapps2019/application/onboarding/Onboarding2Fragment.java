b5c3af59c8f5f508bda30a10da715cf1071c1895
Jeffrey Leung <jleung513@gmail.com>
2019-12-30 23:05:17 -0800

    Dynamically update the current page indicator and onboarding action

--- app/src/main/java/com/pennapps2019/application/onboarding/Onboarding2Fragment.java
+++ app/src/main/java/com/pennapps2019/application/onboarding/Onboarding2Fragment.java
@@ -1,18 +1,13 @@
 package com.pennapps2019.application.onboarding;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-import com.pennapps2019.application.MapsActivity;
 import com.pennapps2019.application.R;
-
-import java.util.Objects;
 
 // Fragment must be a public static class
 public class Onboarding2Fragment extends Fragment {
@@ -20,22 +15,9 @@ public class Onboarding2Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(
+        return inflater.inflate(
                 R.layout.fragment_onboarding_2, container, false
         );
-        linkEndButton(view);
-        return view;
-    }
-
-    private void linkEndButton(View view) {
-        // Set button behaviour
-        final Button button = view.findViewById(R.id.button_finish_onboarding);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(getActivity(), MapsActivity.class));
-                Objects.requireNonNull(getActivity()).finish();
-            }
-        });
     }
 
 }
--- app/src/main/java/com/pennapps2019/application/onboarding/OnboardingActivity.java
+++ app/src/main/java/com/pennapps2019/application/onboarding/OnboardingActivity.java
@@ -1,6 +1,11 @@
 package com.pennapps2019.application.onboarding;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -8,6 +13,7 @@ import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import com.pennapps2019.application.MapsActivity;
 import com.pennapps2019.application.R;
 
 public class OnboardingActivity extends FragmentActivity {
@@ -23,10 +29,65 @@ public class OnboardingActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
 
-        viewPager = findViewById(R.id.activity_onboarding);
+        viewPager = findViewById(R.id.activity_onboarding_content);
 
         // Create adapter to provide the pages to the view pager widget.
         viewPager.setAdapter(new OnboardingFragmentPagerAdapter(this));
+
+        linkActionButton();
+
+        registerCurrentPageAndAction();
+    }
+
+    private void linkActionButton() {
+        // Set button behaviour
+        final Context context = this;
+        final Button button = findViewById(R.id.btn_onboarding_action);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(context, MapsActivity.class));
+                finish();
+            }
+        });
+    }
+
+
+    private void registerCurrentPageAndAction() {
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+//            @Override
+//            public void onPageScrollStateChanged(int arg0) {
+//            }
+//
+//            @Override
+//            public void onPageScrolled(int arg0, float arg1, int arg2) {
+//            }
+
+            @Override
+            public void onPageSelected(int pos) {
+
+                // Set text for action button
+                int lastPage = NUM_PAGES - 1;
+                String text = (pos == lastPage) ?
+                        getString(R.string.finish) :
+                        getString(R.string.skip);
+
+                TextView onboardingAction = findViewById(R.id.btn_onboarding_action);
+                onboardingAction.setText(text);
+
+                // Set page markers
+                View page_0_inactive = findViewById(R.id.onboarding_page_0_indicator);
+                View page_1_inactive = findViewById(R.id.onboarding_page_1_indicator);
+                if (pos == 0) {
+                    page_0_inactive.setVisibility(View.INVISIBLE);
+                    page_1_inactive.setVisibility(View.VISIBLE);
+                }
+                else {
+                    page_0_inactive.setVisibility(View.VISIBLE);
+                    page_1_inactive.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
     }
 
     @Override
